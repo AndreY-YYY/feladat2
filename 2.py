7c862f1c678080cb2c39fc406569aaa94488360f
AndreY-YYY <152394149+AndreY-YYY@users.noreply.github.com>
2025-11-24 09:18:44 +0100

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,18 +1,16 @@
 import requests
 import json
 
-CONTROLLER = "http://192.168.101.254"
-USERNAME = "admin"
-PASSWORD = "admin123"
-
-url = f"{CONTROLLER}/api/v1/ticket"
+url = f"http://192.168.101.254/api/v1/ticket"
 
 payload = {
-    "username": USERNAME,
-    "password": PASSWORD
+    "username": "admin",
+    "password": "admin123"
 }
 
-headers = {"Content-Type": "application/json"}
+headers = {
+    "Content-Type": "application/json"
+}
 
 response = requests.post(url, data=json.dumps(payload), headers=headers)
 
@@ -20,4 +18,30 @@ if response.status_code == 200:
     ticket = response.json()["response"]["serviceTicket"]
     print("Service Ticket:", ticket)
 else:
-    print("Hiba a ticket lekérésekor:", response.text)
+    print("Hiba történt:", response.status_code, response.text)
+
+###################################################
+
+import requests
+
+
+ticket = input("Add meg a service ticket-et: ")
+
+url = f"http://192.168.101.254/api/v1/network-device"
+
+headers = {
+    "X-Auth-Token": ticket
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    devices = response.json()["response"]
+
+    for dev in devices:
+        platform = dev.get("platformId", "N/A")
+        ip = dev.get("managementIpAddress", "N/A")
+        print(f"Platform ID: {platform}, Management IP: {ip}")
+
+else:
+    print("Hiba történt:", response.status_code, response.text)
